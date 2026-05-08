00985efc6fdedd5172ec88d70be5f10bae99abde
Carcharoth00 <pablo.s345701@cesurformacion.com>
2026-05-08 18:22:09 +0200

    mod para vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  socketUrl: '/'
+  apiUrl: 'https://vtt-tfg-production.up.railway.app/api',
+  socketUrl: 'https://vtt-tfg-production.up.railway.app'
 };
